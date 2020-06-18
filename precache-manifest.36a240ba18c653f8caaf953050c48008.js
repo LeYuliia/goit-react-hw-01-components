d52884589a2d72509ecbc9cbe013b372371e068f
LeYuliia <legeza.y@gmail.com>
2020-06-18 10:00:18 +0300

    Updates

---
+++ precache-manifest.36a240ba18c653f8caaf953050c48008.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6959180db064dcb1f6cfc31e9088f944",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "6349a3c273e0db59d1f8",
+    "url": "/goit-react-hw-01-components/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "f47d6170acd3badd744d",
+    "url": "/goit-react-hw-01-components/static/css/main.284e1782.chunk.css"
+  },
+  {
+    "revision": "6349a3c273e0db59d1f8",
+    "url": "/goit-react-hw-01-components/static/js/2.953c2759.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.953c2759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47d6170acd3badd744d",
+    "url": "/goit-react-hw-01-components/static/js/main.b9fc07bf.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
